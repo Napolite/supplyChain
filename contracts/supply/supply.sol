7c0cd05058db1ddc07d984b1f9834e33e0e4af2d
Constantino <Okonemmanuelmma@gmail.com>
2023-06-09 10:17:34 +0100

    added modifiers

--- contracts/supply/supply.sol
+++ contracts/supply/supply.sol
@@ -1,4 +1,20 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-contract SupplyChain {}
+contract SupplyChain {
+    address manufacturer;
+    uint256 totalProducts;
+
+    modifier onlyManufacturer() {
+        require(
+            msg.sender == manufacturer,
+            "Only the manufacturer can call this function."
+        );
+        _;
+    }
+
+    modifier onlyValidProduct(uint256 productId) {
+        require(productId < totalProducts, "Invalid product ID.");
+        _;
+    }
+}
