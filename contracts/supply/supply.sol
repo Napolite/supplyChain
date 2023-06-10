08e4e97b51254549be45ec887c4b385f5834ae8a
Constantino <Okonemmanuelmma@gmail.com>
2023-06-10 16:44:11 +0100

    register product and register manufacturer function

--- contracts/supply/supply.sol
+++ contracts/supply/supply.sol
@@ -1,24 +1,33 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
+import "@openzeppelin/contracts/utils/Strings.sol";
 
 contract SupplyChain {
-    address manufacturer;
-    uint256 totalProducts;
-
-    struct Product{
+    uint256 totalProducts = 0;
+    struct Product {
         string productName;
         string productType;
         string careLevel;
         string currentLocation;
         string depositLocation;
         string receivingLocation;
+        address manufacturer;
     }
 
-    struct manufacturer{}
+    mapping(string => Product) public product;
+    mapping(address => bool) public manufacturers;
 
-    modifier onlyManufacturer() {
+    modifier onlyRegisteredManufacturer() {
         require(
-            msg.sender == manufacturer,
+            manufacturers[msg.sender] == true,
+            "Only manufacturers can call this function"
+        );
+        _;
+    }
+
+    modifier onlyManufacturer(uint productId) {
+        require(
+            msg.sender == product[productId].manufacturer,
             "Only the manufacturer can call this function."
         );
         _;
@@ -29,7 +38,33 @@ contract SupplyChain {
         _;
     }
 
-    function registerProduct external(){
+    event ProductCreated(uint256 productId, address manufacturer);
 
+    function registerManufacturer() public {
+        manufacturers[msg.sender] = true;
+    }
+
+    function registerProduct(
+        string calldata _name,
+        string calldata _type,
+        string calldata _careLevel,
+        string calldata _location,
+        string calldata _start,
+        string calldata _end
+    ) public onlyRegisteredManufacturer {
+        string memory time = Strings.toString(block.timestamp);
+        string memory id = string.concat(_name[0:2], time[3:5]);
+
+        product[id] = Product(
+            _name,
+            _type,
+            _careLevel,
+            _location,
+            _start,
+            _end,
+            msg.sender
+        );
+
+        emit ProductCreated(msg.sender, id);
     }
 }
